39129cf1482896274ff53ecb1ea4369cebcaf61d
Lauren Rodrigues <laroscience@gmail.com>
2017-03-08 13:44:22 +0100

    add initial questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+# Vampire Questionnaire 
+
+p "Hello, there.  What is your name?"
+name = gets.chomp
+
+p "Thank you #{name}."
+p "#{name}, How old are you?"
+age = gets.chomp
+
+p "What year were you born, #{name}?"
+birth_year = gets.chomp
+
+p "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+p "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp
