a1a5c6428b3958fec1e741bb213afe63659107d3
Lauren Rodrigues <laroscience@gmail.com>
2017-03-11 07:16:10 +0100

    add conditional to handle edge cases

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,32 +9,44 @@
     #-advance letter count by one.
 
 def encrypt(password)
-
-letter_count = 0
-
-
-  while letter_count < password.length
-   print password[letter_count].next
-   letter_count +=1
   
+  
+letter_count = 0
+alpha = "abcdefghijklmnopqrstuvwxyz"
+
+
+ while letter_count < password.length
+
+     if password[letter_count].next.include? "aa"
+        p alpha[0]
+    else
+        print password[letter_count].next
+    end
+
+   letter_count +=1
+    
   end
-end 
+
+end
 
 
 
 
 #Decrypt
 
-def decrypt(password)
+def decrypt(password_fix)
 
   alpha = "abcdefghijklmnopqrstuvwxyz"
   letter_count = 0 
 
-  while letter_count < password.length
-    letter = password[letter_count]
+  while letter_count < password_fix.length  
+  
+    letter = password_fix[letter_count]
     number = alpha.index("#{letter}")
     print alpha[number.to_i-1]
     letter_count +=1
   end 
 end 
+
+decrypt(encrypt("swordfish"))
 
