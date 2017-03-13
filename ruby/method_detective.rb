2afb539e3995efc6cc7ea49274fa8169fafe41dc
Lauren Rodrigues <laroscience@gmail.com>
2017-03-13 10:42:03 +0100

    finish assignment

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -13,7 +13,6 @@
 # => “zoom”
 
 "enhance".center(15)
-"enhance".replace "    enhance    "
 # => "    enhance    "
 
 "Stop! You’re under arrest!".upcase
@@ -21,25 +20,25 @@
 # => "STOP! YOU’RE UNDER ARREST!"
 
 "the usual" << " suspects"
-"the usual".replace "the usual suspects"
 #=> "the usual suspects"
 
 " suspects".prepend("the usual")
-" suspects".replace "the usual suspects"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
+"The case of the disappearing last letter".chop!
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# it is the integer ordinal for the letter z
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
