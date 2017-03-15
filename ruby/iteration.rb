f33b367dcc26946a484e08544c2758cd8263c2da
Lauren Rodrigues <laroscience@gmail.com>
2017-03-15 19:43:20 +0100

    complete release 2, questions 1-4 for array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,21 +1,58 @@
+# _______________________RELEASE ONE _______________________
+
 dogs = ["Rio", "Rocky", "Fred", "Ares"]
 dogs.each do |name|
     "#{name} is available for adoption"
 end
- dogs 
+ # p dogs 
 
 dogs.map! do |name|
   name.next
 end
 
- dogs
+ # p dogs
 
 
 cars = {BMW: "X5", Ferrari: "LaFerrari", Lamborghini: "Huracan", Porsche: "GT3"}
 
 cars.each do |make, model|
-  p "#{model} is the best #{make}!"
+ # p "#{model} is the best #{make}!"
 end
 
-cars
+# _______________________REALEASE TWO_______________________
+
+# numbers = [24, 7, 10, 45]
+# numbers.delete_if &:even?
+# p numbers
+
+
+ numbers = [24, 7, 10, 45, 100, 3, 66]
+ numbers.each do |number|
+  if !number.even?
+    # p number
+  end
+end
+
+numbers.each do |number|
+  if number < 20
+    # p number
+  end
+end
+
+numbers.each do 
+  until numbers.length == 3
+    numbers.delete_at(-1)
+  end
+end 
+
+p numbers
+
+
+
+
+
+
+
+
+# the_numbers = {1 => "A", 2 => "B", 3 => "C", 4 => "D"}
 
