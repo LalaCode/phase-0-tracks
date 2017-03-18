49993bc6b8fb57a83ec8b6bb09977d27dca268be
Lauren Rodrigues <laroscience@gmail.com>
2017-03-18 08:53:27 +0100

    Initialize empty hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+#Initialize hash
+client_data = {
+  name: nil,
+  age: nil,
+  children: nil,
+  decor_theme: nil,
+  city: nil,
+  pets: nil,
+  years_at_residence: nil
+
+}
+p client_data
+#Ask user for data
+print "Greetings, I will not prompt you for client details."
+print
+#Convert input to appropriate data type
+#Print hash for user
+#Ask user for updates
+#Print updated hash
