33b96314c33ec5d4599ca56f4955d59a2d55816d
Lauren Rodrigues <laroscience@gmail.com>
2017-03-19 22:18:42 +0100

    Add class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+=begin
+  SPECIES = Puppy
+  CHARACTERISTICS = 
+    Legs = 4
+    Ears = 2
+    Color = varies
+    Friendly = yes
+    Fur length = varies
+  BEHAVIOR = 
+    run
+    jump
+    eat
+    sleep
+=end
