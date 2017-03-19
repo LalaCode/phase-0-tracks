cac34bdff8fd518e5078956642f2be39d177066b
Lauren Rodrigues <laroscience@gmail.com>
2017-03-19 12:00:15 +0100

    Identify vowel code complete

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,15 +1,21 @@
 #swap first and last name
 name = "Felicia Torres"
-name = name.split(' ').reverse!.join(' ')
+name = name.split(' ').reverse!.join(' ').downcase
 # p name
 
 #change vowels to next vowel
 vowel = ["a", "e", "i", "o", "u"]
 consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-name = name.downcase.split('')
+name = name.split('')
+# p name
+index_count = 0
 
 name.each do |letter|
-  p letter
+  vowel.each do |vowel|
+    if letter == vowel
+      p vowel
+    end
+  end
 end
 
-#change consonant to next consonant
+    
