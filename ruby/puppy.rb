6aa0a417dd5d2fa12d4d90f78ad3540dc4051afb
Lauren Rodrigues <laroscience@gmail.com>
2017-03-19 22:30:30 +0100

    Finish class design

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,11 +1,13 @@
 =begin
   SPECIES = Puppy
+
   CHARACTERISTICS = 
     Legs = 4
     Ears = 2
     Color = varies
     Friendly = yes
     Fur length = varies
+    
   BEHAVIOR = 
     run
     jump
