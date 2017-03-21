af1467f0446991ef90c57fa093d2f5b62593ae37
Lauren Rodrigues <laroscience@gmail.com>
2017-03-21 09:21:03 +0100

    Experiment with driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,4 @@
+
 class Santa
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
@@ -5,6 +6,8 @@ class Santa
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
+
+    puts "This santa is a #{ethnicity} #{gender} santa."
   end
 
   def speak
@@ -17,7 +20,20 @@ class Santa
 
 end
 
-american = Santa.new
-american.speak
-american.eat_milk_and_cookies("oatmeal")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+
+santas.each do |santa|
+  santa.speak
+  santa.eat_milk_and_cookies("Chocolate")
+end
+
+
+
 
