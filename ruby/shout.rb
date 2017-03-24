abf8938e3746ce081e2f1dbfa46167d66ee95d4d
Lauren Rodrigues <laroscience@gmail.com>
2017-03-24 10:58:31 +0100

    Write Shout methods and driver code successfully

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+def self.yell_happily(words)
+  words + "!!!" + " I'm so happy!"
 end
+end
+
+puts Shout.yell_angrily("I'm so angry")
+puts Shout.yell_happily("This is great")
