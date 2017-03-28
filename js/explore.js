e78d90bff941469c4f25ec5d4addb7fdb979268f
Lauren Rodrigues <laroscience@gmail.com>
2017-03-28 11:23:13 +0200

    Add and test reverse function successfully

--- js/explore.js
+++ js/explore.js
@@ -1,8 +1,28 @@
 //PSUEDOCODE:
 
-//Take string
-//separate letters
-//flip the order
-//put the letters back together
-//store the result in a variable 
+//Initiate an empty string that will hold the reversed string
+//Take string as argument in function
+//Iterate over each letter starting with the last letter
+  //identify letter by the position the string
+  //while the string is larger than -1 the loop should continue
+  //subtract 1 from letter index
+  //add that letter to the empty string
+  
 //print variable based on meeting a condition
+
+var string = "hello"
+var newString = "";
+var sunny = true
+
+function reverse(word){
+
+  for (var i = word.length-1; i>-1; i--) {
+    newString = newString + word[i];
+  };
+  console.log(newString)
+
+};
+
+reverse("hello")
+
+
