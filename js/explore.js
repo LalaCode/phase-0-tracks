6a4887901f961b66fc771b38ba4453eb22bd1fab
Lauren Rodrigues <laroscience@gmail.com>
2017-03-28 10:36:53 +0200

    Initial commit

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+//PSUEDOCODE:
+
+//Take string
+//separate letters
+//flip the order
+//put the letters back together
+//store the result in a variable 
+//print variable based on meeting a condition
