f20e46d7f948a322e03b313d7eeab9c451d91483
Lauren Rodrigues <laroscience@gmail.com>
2017-03-31 19:44:17 +0200

    Initial commit

---
+++ js/algos.js
@@ -0,0 +1,18 @@
+//iterate through the array to test the length of each one 
+//compare length to variable length, update if next word is longer
+//print the one with longest length
+
+var word_array = ["banana", "elementary", "disco", "unnatural", "uberhauptnicht"];
+var longest_word = null;
+
+function Word_Selector(word_array){
+  for (var i=0, i<word_array.length, i++) {
+
+    if (word_array[i].length > longest_word) {
+      
+    };
+
+  };
+
+} 
+
