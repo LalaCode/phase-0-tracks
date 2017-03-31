e9fe7b8c9102769c4a469cc953d2c8d15ead505c
Lauren Rodrigues <laroscience@gmail.com>
2017-03-31 21:33:00 +0200

    Initial commit

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,36 +1,17 @@
-var horse_colors = ["brown", "black", "white", "blue"];
-var horse_names = ["Piper", "Judy", "Birke", "Fred"];
+var horseColors = ["Black", "White", "Brown", "Orange"];
 
+var horseNames = ["Fred", "Jenny", "Bob", "Seabiscuit"];
 
-horse_colors.push("orange");
-horse_names.push("Lovey");
+horseColors.push("Grey");
+horseNames.push("Mr. Ed");
 
-var horse_info = {}
+console.log(horseColors);
+console.log(horseNames);
 
-for (var i=0; i < horse_names.length; i++ ) {horse_info[horse_names[i]] = horse_colors[i]};
+horses = {};
 
-console.log(horse_info)
-
-//car constructor function
-
-function Car(make,model,transmission){
-
-  this.make = make;
-  this.model = model;
-  this.transmission = transmission;
-  this.engine_rev = function() { console.log("Vroom vroom vroom!"); };
-  
+for (var i = 0; i < horseNames.length; i++) {
+  horses[horseNames[i]] = horseColors[i];
 }
 
-var firstCar = new Car("BMW", "X5", "automatic");
-console.log(firstCar);
-firstCar.engine_rev();
-
-var secondCar = new Car("Mercedes", "G Wagon", "automatic");
-console.log(secondCar);
-secondCar.engine_rev();
-
-//literal constructor for car
-
-var car = {make: 'Audi', model: 'TT', transmission: 'manual'}
-console.log(car)
+console.log(horses);
