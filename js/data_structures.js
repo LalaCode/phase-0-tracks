5f973dce46db39a1fc68e0a525b31b6565de4de1
Lauren Rodrigues <laroscience@gmail.com>
2017-03-31 11:52:00 +0200

    Create horse info object and iterate through the arrays to populate it

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,9 @@ var horse_names = ["Piper", "Judy", "Birke", "Fred"];
 
 horse_colors.push("orange");
 horse_names.push("Lovey");
+
+var horse_info = {}
+
+for (var i=0; i < horse_names.length; i++ ) {horse_info[horse_names[i]] = horse_colors[i]};
+
+console.log(horse_info)
