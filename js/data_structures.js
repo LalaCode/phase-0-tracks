fd2b860b69354db8a613eeb4da68e4ef16253c3d
Lauren Rodrigues <laroscience@gmail.com>
2017-03-31 21:47:02 +0200

    Successfully add constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,5 @@
+//Horse arrays and objects
+
 var horseColors = ["Black", "White", "Brown", "Orange"];
 
 var horseNames = ["Fred", "Jenny", "Bob", "Seabiscuit"];
@@ -15,3 +17,18 @@ for (var i = 0; i < horseNames.length; i++) {
 }
 
 console.log(horses);
+
+//Car Constructor
+
+function Car (make, model, year, used) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.used = used;
+  this.honk = function(){console.log("HONK! HONK!");};
+
+}
+
+var firstCar = new Car("Toyota", "Camry", 1994, true);
+console.log(firstCar);
+firstCar.honk();
