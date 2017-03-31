e2d0c60c16ebd429fff8658a4de1c08cee517640
Lauren Rodrigues <laroscience@gmail.com>
2017-03-31 14:24:20 +0200

    Finalize and successfully test car constructor code

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,3 +10,27 @@ var horse_info = {}
 for (var i=0; i < horse_names.length; i++ ) {horse_info[horse_names[i]] = horse_colors[i]};
 
 console.log(horse_info)
+
+//car constructor function
+
+function Car(make,model,transmission){
+
+  this.make = make;
+  this.model = model;
+  this.transmission = transmission;
+  this.engine_rev = function() { console.log("Vroom vroom vroom!"); };
+  
+}
+
+var firstCar = new Car("BMW", "X5", "automatic");
+console.log(firstCar);
+firstCar.engine_rev();
+
+var secondCar = new Car("Mercedes", "G Wagon", "automatic");
+console.log(secondCar);
+secondCar.engine_rev();
+
+//literal constructor for car
+
+var car = {make: 'Audi', model: 'TT', transmission: 'manual'}
+console.log(car)
