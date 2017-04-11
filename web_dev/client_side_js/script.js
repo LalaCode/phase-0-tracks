7f29cab135ebcf7f08ad849a7ad509bd4693961a
Lauren Rodrigues <laroscience@gmail.com>
2017-04-11 19:50:53 +0200

    Add script functionality

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,9 @@
 console.log("Testing testing 1 2 3");
+
+function headingChanger(){
+  console.log("Heading Changer!");
+  event.target.style.fontStyle="italic";
+}
+
+var heading = document.querySelector("h1");
+heading.addEventListener("mouseover", headingChanger);
